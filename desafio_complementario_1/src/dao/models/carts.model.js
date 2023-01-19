072ea95a409e0b4d9c8aa239ee4d5ca596621252
Ignacio Rossetto <ignaciorossetto@hotmail.com>
2023-01-19 08:59:06 -0300

    Cart works with localStorage. I`ll start working with db populate

--- desafio_complementario_1/src/dao/models/carts.model.js
+++ desafio_complementario_1/src/dao/models/carts.model.js
@@ -6,64 +6,59 @@ const cartsCollection = "carts";
 // Schema definition
 
 const productCartSchema = new mongoose.Schema({
-  _id: {
-    type: String,
-    required: true,
-  },
-  quantity: {
-    type: Number,
-    required: true,
-  },
-});
+    _id: {
+        type: String,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    }
+})
 
 const cartSchema = new mongoose.Schema({
-  shippingInfo: {
-    name: {
-      type: String,
-      required: true,
-    },
-    address: {
-      type: String,
-      required: true,
-    },
-    id: {
-      type: Number,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-    },
-    cellphone: {
-      type: String,
-      required: true,
-    },
-  },
-  paymentInfo: {
-    method: {
-      type: String,
-      required: true,
-    },
-    status: {
-      type: String,
-      required: true,
-    },
-  },
-  cart: {
-    type: [
-      {
-        product: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "products",
+    shippingInfo: {
+        name: {
+            type: String,
+            required: true
         },
-        quantity: Number,
-      },
-    ],
-    required: true,
-    default: undefined,
-    _id: false,
-  },
+        address: {
+            type: String,
+            required: true
+        },
+        id: {
+            type: Number,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        cellphone: {
+            type: String,
+            required: true
+        },
+    },
+    paymentInfo: {
+        method: {
+            type: String,
+            required: true
+        },
+        status: {
+            type: String,
+            required: true
+        },
+    },
+    cart: {
+        type: [productCartSchema],
+        required: true,
+        default: undefined,
+        _id : false
+    }
 });
+
+
+
 
 // Model creation. Collection + Schema
 export const cartModel = mongoose.model(cartsCollection, cartSchema);
